8d7e349a0abb78c307c2df86168c80e81ac03764
Kimberly Hsiao <kimberly.hsiao@gmail.com>
2014-01-08 22:06:25 -0800

    Initial Commit

---
+++ BlinkIt/BIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIAppDelegate.h
+//  BlinkIt
+//
+//  Created by Kimberly Hsiao on 1/8/14.
+//  Copyright (c) 2014 hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlinkIt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlinkIt
+//
+//  Created by Kimberly Hsiao on 1/8/14.
+//  Copyright (c) 2014 hsiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIAppDelegate class]));
+    }
+}
