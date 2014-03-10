a47b479ffb1736eaef5137d1d87eb2cfadd7c4e6
Kimberly Hsiao <kimberly.hsiao@gmail.com>
2014-03-10 01:09:46 -0700

    use cached prof pic everywhere

--- BlinkIt/BIPendingRequestTableViewCell.m
+++ BlinkIt/BIPendingRequestTableViewCell.m
@@ -53,15 +53,23 @@
         [self setupActionButtonNormally];
     }
     
-    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0ul);
-    
-    dispatch_async(queue, ^{
-        UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:user[@"photoURL"]]]];
+    NSString *photoURL = user[@"photoURL"];
+    UIImage *profPic = [[BIFileSystemImageCache shared] objectForKey:photoURL];
+    if (profPic) {
+        _profilePic.image = profPic;
+    } else if (photoURL.hasContent){
+        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0ul);
         
-        dispatch_sync(dispatch_get_main_queue(), ^{
-            _profilePic.image = image;
+        dispatch_async(queue, ^{
+            UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:user[@"photoURL"]]]];
+            
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                _profilePic.image = image;
+                [_profilePic fadeInWithDuration:0.2 completion:nil];
+                [[BIFileSystemImageCache shared] setObject:image forKey:photoURL];
+            });
         });
-    });
+    }
 }
 
 - (void)setupActionButtonNormally {
